750da94def950c1a042ea9a501ab02edbb5e9f8f
jriv95 <joel.rivera7995@gmail.com>
2019-01-28 15:24:59 -0500

    Played around a bit with the assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./Components/Header.js";
+import Header from "./Components/Header"
 
 class App extends Component {
   render() {
-    // the fact that this is being returned inside of App that makes it the parent
-    return <Header header="Test" />;
+    // the fact that this is being returned inside of App, that makes it the parent
+    return (
+    < Header 
+    header="This is the header prop"
+    somekey="value"   
+    anotherkey="another value"/>
+    );
   }
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,14 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    return <h1>{this.props.header}</h1>;
+    return (
+      <div>
+        <h1>{this.props.header}</h1>
+        <h1>{this.props.somekey}</h1>
+        <p>{this.props.anotherkey}</p>
+        <p>What is I didnt want a prop key and I just wanted this to show</p>
+      </div>
+      );
   }
 }
 
