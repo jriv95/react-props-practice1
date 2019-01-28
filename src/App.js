42f60389724444ede1550afbbde6e31febf556d0
jriv95 <joel.rivera7995@gmail.com>
2019-01-28 14:55:48 -0500

    First run through

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./Components/Header.js";
 
 class App extends Component {
   render() {
-    return <header />;
+    // the fact that this is being returned inside of App that makes it the parent
+    return <Header header="Test" />;
   }
 }
 
